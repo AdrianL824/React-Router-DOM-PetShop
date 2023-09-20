332d426862cdf5d7616a0218f40cbff0eef9b6f3
AdrianL824 <adrian.lopez.or2408@gmail.com>
2023-09-20 17:16:45 -0400

    React Router DOM Inicio proyecto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+import Home from "./pages/Home.jsx";
+import Sobre from "./pages/Sobre.jsx";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Sobre />
+    </>
+  );
+}
+
+export default App;
